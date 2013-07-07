fad6ec709b3a1fa5ffaf201d8e76f82d9f255592
Rafael Rezende <rafaelrezend@gmail.com>
2013-07-07 21:02:34 +0200

    Revert "Test"
    This reverts commit 49cda4243b414ba63241d0e0e5c3a3a80005e99c.

--- MiscScripts/Matlab/image-transfer-via-audio/ber.m
+++
@@ -1,9 +0,0 @@
-%This function calculates the error rate between original signal and
-%demodulated signal.
-
-function [rate] = ber(original, demodulated)
-
-    wrong_bits = sum(xor(original,demodulated));
-    rate = wrong_bits/length(original)*100;
-
-end
--- MiscScripts/Perl/analysis-parser/README.txt
+++
@@ -1,4 +0,0 @@
-PT JOB
-
-Code written and packed in Linux.
-Please unpack it also ON Linux before working.
